cdf73f8d4442fe5fda7ab25b1e84b6aad96d388b
moen <moen@366.hopto.org>
2013-05-02 19:51:35 +0200

    Increased version number.

--- src/monnef/core/Reference.java
+++ src/monnef/core/Reference.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Reference {
-    public static final String Version = "0.4.15";
+    public static final String Version = "0.4.16";
     public static final String ModId = "monnef-core";
     public static final String ModName = "monnef's Core";
     public static final String URL_JAFFAS = "http://jaffas.moxo.cz"; // without trailing slash
