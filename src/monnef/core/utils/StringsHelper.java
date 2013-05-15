4f002248239b76af10bb1e3157c4f4365709cf6e
moen <moen@366.hopto.org>
2013-05-15 21:31:27 +0200

    Moved some helper methods from Jaffas to the core.

---
+++ src/monnef/core/utils/StringsHelper.java
@@ -0,0 +1,41 @@
+/*
+ * Jaffas and more!
+ * author: monnef
+ */
+
+package monnef.core.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class StringsHelper {
+    public static String getMD5(String input) {
+        byte[] bytesOfMessage = new byte[0];
+        try {
+            bytesOfMessage = input.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        byte[] thedigest = null;
+        if (md != null) {
+            thedigest = md.digest(bytesOfMessage);
+        }
+
+        return md == null ? null : toHex(thedigest);
+    }
+
+    public static String toHex(byte[] bytes) {
+        BigInteger bi = new BigInteger(1, bytes);
+        return String.format("%0" + (bytes.length << 1) + "X", bi);
+    }
+}
