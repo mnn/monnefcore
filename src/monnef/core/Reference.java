796f57847c4b9009db92f00fa73b611733826981
moen <moen@366.hopto.org>
2013-05-17 17:15:36 +0200

    Bumped core version.

--- src/monnef/core/Reference.java
+++ src/monnef/core/Reference.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Reference {
-    public static final String Version = "0.4.17";
+    public static final String Version = "0.4.18";
     public static final String ModId = "monnef-core";
     public static final String ModName = "monnef's Core";
     public static final String URL_JAFFAS = "http://jaffasfiles.moxo.cz"; // without trailing slash
