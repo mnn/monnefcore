b59c9a5ec3e8d0075b8cdf8a5639680b116fd3e9
moen <moen@366.hopto.org>
2013-05-24 22:36:50 +0200

    Implemented helper class Interval.

---
+++ src/monnef/core/utils/Interval.java
@@ -0,0 +1,41 @@
+/*
+ * Jaffas and more!
+ * author: monnef
+ */
+
+package monnef.core.utils;
+
+import java.util.ArrayList;
+
+public class Interval {
+    private final int min;
+    private final int max;
+
+    public Interval(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public static Interval[] fromArray(int... items) {
+        if (items.length <= 0) {
+            throw new RuntimeException("Incorrect count of parameters.");
+        }
+        if (items.length % 2 == 1) {
+            throw new RuntimeException("Odd count of parameters.");
+        }
+
+        ArrayList<Interval> tmp = new ArrayList<Interval>();
+        for (int i = 0; i < items.length; i += 2) {
+            tmp.add(new Interval(items[i], items[i + 1]));
+        }
+        return tmp.toArray(new Interval[]{});
+    }
+}
