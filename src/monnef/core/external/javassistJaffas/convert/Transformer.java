46c06030def0db1c162e6d85dbbd79ca78469238
moen <moen@366.hopto.org>
2013-07-18 16:23:14 +0200

    Moved javassist to a nicer package.

---
+++ src/monnef/core/external/javassistJaffas/convert/Transformer.java
@@ -0,0 +1,57 @@
+/*
+ * Javassist, a Java-bytecode translator toolkit.
+ * Copyright (C) 1999- Shigeru Chiba. All Rights Reserved.
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License.  Alternatively, the contents of this file may be used under
+ * the terms of the GNU Lesser General Public License Version 2.1 or later,
+ * or the Apache License Version 2.0.
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ */
+
+package monnef.core.external.javassistJaffas.convert;
+
+import monnef.core.external.javassistJaffas.CannotCompileException;
+import monnef.core.external.javassistJaffas.CtClass;
+import monnef.core.external.javassistJaffas.bytecode.BadBytecode;
+import monnef.core.external.javassistJaffas.bytecode.CodeAttribute;
+import monnef.core.external.javassistJaffas.bytecode.CodeIterator;
+import monnef.core.external.javassistJaffas.bytecode.ConstPool;
+import monnef.core.external.javassistJaffas.bytecode.MethodInfo;
+import monnef.core.external.javassistJaffas.bytecode.Opcode;
+
+/**
+ * Transformer and its subclasses are used for executing
+ * code transformation specified by CodeConverter.
+ *
+ * @see monnef.core.external.javassistJaffas.CodeConverter
+ */
+public abstract class Transformer implements Opcode {
+    private Transformer next;
+
+    public Transformer(Transformer t) {
+        next = t;
+    }
+
+    public Transformer getNext() { return next; }
+
+    public void initialize(ConstPool cp, CodeAttribute attr) {}
+    
+    public void initialize(ConstPool cp, CtClass clazz, MethodInfo minfo) throws CannotCompileException { 
+    	initialize(cp, minfo.getCodeAttribute());
+    }
+
+    public void clean() {}
+
+    public abstract int transform(CtClass clazz, int pos, CodeIterator it,
+                ConstPool cp) throws CannotCompileException, BadBytecode;
+
+    public int extraLocals() { return 0; }
+
+    public int extraStack() { return 0; }
+}
