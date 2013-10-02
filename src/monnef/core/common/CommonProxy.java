0dba65c4971474c5320310914182ee326b113ab1
moen <moen@366.hopto.org>
2013-10-02 20:56:09 +0200

    Added debug message to server proxy (possible scala issue).

--- src/monnef/core/common/CommonProxy.java
+++ src/monnef/core/common/CommonProxy.java
@@ -5,11 +5,12 @@
 
 package monnef.core.common;
 
+import monnef.core.MonnefCorePlugin;
 import net.minecraft.world.World;
 
 public class CommonProxy {
-    public void registerClientStuff(){
-
+    public void registerClientStuff() {
+        MonnefCorePlugin.Log.printDebug("rCS on server");
     }
 
     public World getClientWorld() {
