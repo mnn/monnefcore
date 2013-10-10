0cb4b8a5f76e3ba0fd7facd792518df59a4f2b36
moen <moen@366.hopto.org>
2013-10-10 23:12:24 +0200

    Added pipe operator for small tuples.

--- src/monnef/core/utils/scala/package.scala
+++ src/monnef/core/utils/scala/package.scala
@@ -5,10 +5,26 @@
 
 package monnef.core.utils
 
-package object scala {
+package object scalautils {
 
   implicit class PipedObject[A](value: A) {
     def |>[B](f: A => B): B = f(value)
+  }
+
+  implicit class PipedObjectTuple1[A](value: (A)) {
+    def |>[B](f: (A) => B): B = f(value)
+  }
+
+  implicit class PipedObjectTuple2[A, B](value: (A, B)) {
+    def |>[O](f: (A, B) => O): O = f(value._1, value._2)
+  }
+
+  implicit class PipedObjectTuple3[A, B, C](value: (A, B, C)) {
+    def |>[O](f: (A, B, C) => O): O = f(value._1, value._2, value._3)
+  }
+
+  implicit class PipedObjectTuple4[A, B, C, D](value: (A, B, C, D)) {
+    def |>[O](f: (A, B, C, D) => O): O = f(value._1, value._2, value._3, value._4)
   }
 
 }
