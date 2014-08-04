f3364db6e067043fa6f733c5c3b11d403548014a
moen <moen@366.hopto.org>
2014-08-04 18:48:23 +0200

    Cleaned unused imports.

--- src/main/java/monnef/core/block/TileContainerMonnefCore.java
+++ src/main/java/monnef/core/block/TileContainerMonnefCore.java
@@ -5,17 +5,11 @@
 
 package monnef.core.block;
 
-import monnef.core.MonnefCorePlugin;
 import monnef.core.common.ContainerRegistry;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.inventory.Container;
 import net.minecraft.inventory.IInventory;
-import net.minecraft.inventory.Slot;
-import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-
-import java.util.List;
 
 public abstract class TileContainerMonnefCore extends ContainerMonnefCore {
 
