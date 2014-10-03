bc89c547e5b9c8edabee708fc49a7de2eadde461
moen <moen@366.hopto.org>
2014-10-03 22:50:37 +0200

    Implemented basic sash rendering.

--- src/main/scala/monnef/core/client/CustomPlayerRenderer.scala
+++ src/main/scala/monnef/core/client/CustomPlayerRenderer.scala
@@ -1,12 +1,28 @@
 package monnef.core.client
 
-import net.minecraft.client.model.ModelBiped
+import net.minecraft.client.model.{ModelRenderer, ModelBiped}
 import net.minecraftforge.client.event.RenderPlayerEvent
 import cpw.mods.fml.common.eventhandler.SubscribeEvent
 import monnef.core.mod.MonnefCoreNormalMod
+import cpw.mods.fml.common.FMLCommonHandler
+import cpw.mods.fml.client.FMLClientHandler
+import monnef.core.client.ResourcePathHelper.ResourceTextureType
+import net.minecraft.util.ResourceLocation
+import monnef.core.Reference
 
 class CustomPlayerRenderer {
-  val modelOver = new ModelBiped(1.0F)
+
+  import CustomPlayerRenderer._
+
+  val texture = new ResourceLocation(ResourcePathHelper.assemble("sash_1.png", Reference.ModName, ResourceTextureType.ENTITY))
+
+  val modelOver = new ModelBiped(1.0F, 0, TEXTURE_WIDTH, TEXTURE_HEIGHT) {
+    isChild = false
+
+    def disablePart(part: ModelRenderer) { part.showModel = false }
+
+    Seq(bipedHead, bipedHeadwear, bipedCloak, bipedEars, bipedLeftArm, bipedLeftLeg, bipedRightArm, bipedRightLeg).foreach(disablePart)
+  }
 
   @SubscribeEvent
   def onPostRenderSpecials(event: RenderPlayerEvent.Specials.Post) {
@@ -14,7 +30,14 @@ class CustomPlayerRenderer {
     if (sashNumber != 0) {
       // setRotationAngles(partial tick, arm, arm max, headRotY, headRotX, ???, entity)
       // render(entity, partial tick, arm, arm max, headRotY, headRotX, ?scale?)
-      modelOver.render(event.entity, event.partialRenderTick, 0, 0, 0, 0, 1)
+      FMLClientHandler.instance.getClient.renderEngine.bindTexture(texture)
+      val player = event.entityPlayer
+      modelOver.render(player, event.partialRenderTick, 0, 0, 0, 0, 1f / 16)
     }
   }
+}
+
+object CustomPlayerRenderer {
+  final val TEXTURE_WIDTH = 64
+  final val TEXTURE_HEIGHT = 32
 }
